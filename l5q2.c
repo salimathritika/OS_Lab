5baa4d7e97afa762a23e815a56210e9c84eac7c5
salimathritika <126080853+salimathritika@users.noreply.github.com>
2024-02-12 10:18:13 +0530

    Add files via upload

--- l5q2.c
+++ l5q2.c
@@ -1,58 +1,58 @@
-//copied
-#include <string.h>
+//lab 5 question 2 working
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <string1> <string2> ... <stringN>\n", argv[0]);
-        return 1;
-    }
 
-    pid_t pid = fork();
+int main(int argc, char *argv[])
+{
+if(argc==1)
+{printf("No extra arguements passed");
+return 0;
+}
+pid_t p;
+int status;
+p=fork();
+if(p>0)
+{
+wait(&status);
 
-    if (pid < 0) {
-        fprintf(stderr, "Fork failed\n");
-        return 1;
-    } else if (pid == 0) { // Child process
-        // Sort the strings
-        int num_strings = argc - 1;
-        char *sorted_strings[num_strings];
+printf("Unsorted list by the parent process\n");
+for(int i=1;i<argc;i++)
+printf("%s\t",argv[i]);
+printf("\n");
+}
+else if(p==0)
+{
+printf("Sorted list by the child process:\n");
+int num = argc - 1;
+char *sorted[num];
+for (int i = 0; i < num; i++) 
+{
+sorted[i] = argv[i + 1];
+}
+for (int i = 0; i < num - 1; i++) 
+{
+for (int j = i + 1; j < num; j++) 
+{
+if (strcmp(sorted[i], sorted[j]) > 0) 
+{
+   char *temp = sorted[i];
+   sorted[i] = sorted[j];
+   sorted[j] = temp;
+}
+}
+}
 
-        // Copy the strings into a new array
-        for (int i = 0; i < num_strings; i++) {
-            sorted_strings[i] = argv[i + 1];
-        }
-
-        // Sort the array of strings
-        for (int i = 0; i < num_strings - 1; i++) {
-            for (int j = i + 1; j < num_strings; j++) {
-                if (strcmp(sorted_strings[i], sorted_strings[j]) > 0) {
-                    char *temp = sorted_strings[i];
-                    sorted_strings[i] = sorted_strings[j];
-                    sorted_strings[j] = temp;
-                }
-            }
-        }
-
-        // Display the sorted strings
-        printf("Sorted strings:\n");
-        for (int i = 0; i < num_strings; i++) {
-            printf("%s\n", sorted_strings[i]);
-        }
-
-    } else { // Parent process
-        // Wait for the child process to complete
-        wait(NULL);
-
-        // Display the unsorted strings
-        printf("\nUnsorted strings:\n");
-        for (int i = 1; i < argc; i++) {
-            printf("%s\n", argv[i]);
-        }
-    }
-
-    return 0;
+for(int i=0;i<num;i++)
+printf("%s\t",sorted[i]);
+printf("\n");
+}
+else
+{
+printf("Error. Forking failed");
+}
 }
