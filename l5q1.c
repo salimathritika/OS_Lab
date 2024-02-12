39f7c85e03ba08ce002e6763b81ed5309bf28758
salimathritika <126080853+salimathritika@users.noreply.github.com>
2024-02-12 09:26:00 +0530

    Add files via upload

--- l5q1.c
+++ l5q1.c
@@ -1,23 +1,22 @@
-//copied
+//lab 5 question 1 working
+
 #include <stdio.h>
+#include <sys/types.h>
 #include <unistd.h>
 
-int main() {
-    pid_t pid;
-
-    // Fork a child process
-    pid = fork();
-
-    if (pid < 0) {
-        fprintf(stderr, "Fork failed\n");
-        return 1;
-    } else if (pid == 0) { // Child process
-        printf("Child process: PID = %d, PPID = %d\n", getpid(), getppid());
-        printf("Child process message: Hello from the child!\n");
-    } else { // Parent process
-        printf("Parent process: PID = %d, PPID = %d\n", getpid(), getppid());
-        printf("Parent process message: Hello from the parent!\n");
-    }
-
-    return 0;
+int main()
+{
+pid_t p=fork();
+if(p==0)
+{
+printf("This is the child process\n");
+printf("PID=%d and PPID=%d\n",getpid(),getppid());
+}
+else if(p>0)
+{
+printf("This is the parent process\n");
+printf("PID=%d and PPID=%d\n",getpid(),getppid());
+}
+else
+printf("There was an error in forking");
 }
