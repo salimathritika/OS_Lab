0a6f1435789dc5b1a392a01a34f5fdfa05c9043d
salimathritika <126080853+salimathritika@users.noreply.github.com>
2024-02-12 11:21:26 +0530

    Add files via upload

---
+++ sjf.c
@@ -0,0 +1,24 @@
+//SHORTEST JOB FIRST not working
+#include <stdio.h>
+
+int size,f=0,r=0;
+
+
+
+int main()
+{
+printf("Enter the total number of processes");
+scanf("%d",&size);
+int info[size][2];
+int i,j;
+printf("Enter the process ID, burst time :\n");
+for(i=0;i<size;i++)
+{
+printf("%dth info",(i+1));
+scanf("%d%d",&info[i][0],&info[i][1]);
+}
+
+
+
+}
+
