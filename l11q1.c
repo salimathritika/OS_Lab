101da28a399dc04003f048f01eb197f384c5e743
salimathritika <126080853+salimathritika@users.noreply.github.com>
2024-04-13 19:58:18 +0530

    Add files via upload

--- l11q1.c
+++ l11q1.c
@@ -1,4 +1,5 @@
-//FCFS AND SSTF WORKS
+//FCFS SSTF WORKS
+//SCAN is logically wrong i think
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -8,6 +9,18 @@ typedef struct
 	int request_track_number;
 	bool visited;
 }request;
+
+int absoluteValue(int x)
+{
+    if(x>0)
+    {
+        return x;
+    }
+    else
+    {
+        return x*-1;
+    }
+}
 
 int fcfs(int init,int req[],int n)
 {
@@ -54,95 +67,91 @@ void sstf(int init,request req[],int n)
 	printf("\nSeek Time: %d\n", seek);
 }
 
-/*
-void scan(int init,int prev,request req[],int n,int limit)
+void scan(int init,int n,int max)
 {
-  int choice,i,j;
-  if(prev - init > 0 )
-	{
-		choice = 2;
-	}
-	else
-		choice = 1;
-	
-	int seek=0;
-	printf("%d -> ",init );
-	if(choice == 1)
-	{
-		for(i=init;i<limit;i++)
-		{
-			for(j=0;j<n;j++)
-			{
-				if(req[j].request_track_number == i && req[j].visited == false)
-				{
-					printf("%d -> ", req[j].request_track_number);
-					req[j].visited = true;
-					seek += abs(req[j].request_track_number - init);
-					init = req[j].request_track_number;
-				}
-			}
-		}
-		printf("%d -> ", limit-1);
-		seek += abs(limit-1 - init);
-		init = limit-1;
-		for(i=init;i>=0;i--)
-		{
-			for(j=0;j<n;j++)
-			{
-				if(req[j].request_track_number == i && req[j].visited == false)
-				{
-					printf("%d -> ", req[j].request_track_number);
-					req[j].visited = true;
-					seek += abs(req[j].request_track_number - init);
-					init = req[j].request_track_number;
-				}
-			}
-		}
-		seek += abs(init - 0);
-		printf("0 \n");
-	}
-	else if(choice == 2)
-	{
-		for(i=init;i>=0;i--)
-		{
-			for(j=0;j<n;j++)
-			{
-				if(req[j].request_track_number == i && req[j].visited == false)
-				{
-					printf("%d -> ", req[j].request_track_number);
-					req[j].visited = true;
-					seek += abs(req[j].request_track_number - init);
-					init = req[j].request_track_number;
-				}
-			}
-		}
-		printf("%d -> ", 0);
-		seek += abs(0 - init);
-		init = 0;
-		for(i=init;i<limit;i++)
-		{
-			for(j=0;j<n;j++)
-			{
-				if(req[j].request_track_number == i && req[j].visited == false)
-				{
-					printf("%d -> ", req[j].request_track_number);
-					req[j].visited = true;
-					seek += abs(req[j].request_track_number - init);
-					init = req[j].request_track_number;
-				}
-			}
-		}
-		seek += abs(limit-1 - init );
-		printf("%d \n", limit-1);
-		
-	}
-	printf("Seek Time: %d\n", seek);
+    int queue[25],i,j,k,difference,temp,queue1[20],queue2[20],temp1=0,temp2=0,seek=0;
+    float averageSeekTime;
+    printf("Enter the disk positions to be read(queue): ");
+    for(i=1;i<=n;i++)   
+    {
+        scanf("%d",&temp);         
+        
+        if(temp>init)
+        {
+            queue1[temp1]=temp; 
+            temp1++; 
+        }
+        else    
+        {   
+            queue2[temp2]=temp; 
+            temp2++;
+        }
+    }
+    
+    for(i=0;i<temp1-1;i++)
+    {
+        for(j=i+1;j<temp1;j++)
+        {
+            if(queue1[i]>queue1[j])
+            {
+                temp=queue1[i];
+                queue1[i]=queue1[j];
+                queue1[j]=temp;
+            }
+        }
+    }
+    
+    for(i=0;i<temp2-1;i++)
+    {
+        for(j=i+1;j<temp2;j++)
+        {
+            if(queue2[i]<queue2[j])
+            {
+                temp=queue2[i];
+                queue2[i]=queue2[j];
+                queue2[j]=temp;
+            }
+        }
+    }    
+    
+    for(i=1,j=0;j<temp1;i++,j++)
+    {
+        queue[i]=queue1[j]; 
+    }
+    
+    queue[i]=max;
+    
+    for(i=temp1+2,j=0;j<temp2;i++,j++)
+    {
+        queue[i]=queue2[j];
+    }
+    
+    queue[i]=0;
+ 
+    queue[0]=init;
+    
+    
+    for(j=0; j<=n; j++) 
+    {   
+        
+        difference = absoluteValue(queue[j+1]-queue[j]);
+        seek = seek + difference;
+        
+        printf("Disk head moves from position %d to %d with Seek %d \n",
+        queue[j], queue[j+1], difference);
+    }
+    
+    averageSeekTime = seek/(float)n;
+    
+    printf("Total Seek Time= %d\n", seek);
+    printf("Average Seek Time= %f\n", averageSeekTime);
+
 }
-*/
+
 
 int main()
 {
-   int i,n,init,opt,prev,limit;
+   int i,n,init,opt,prev,limit,maxrange;
    
    printf("\nChoose one of the following algorithms:\n");
    printf("1.FCFS\n2.SSTF\n3.SCAN\n");
@@ -178,24 +187,16 @@ int main()
      }   
      case 3:
      {
-       printf("Enter the number of requests: ");
-	scanf("%d",&n);
-	request req[n];
-	printf("Enter the requests: ");
-	for (i = 0; i < n; ++i)
-	{
-		scanf("%d",&req[i].request_track_number);
-		req[i].visited = false;
-	}
-	printf("Enter initial position of head: ");
-	scanf("%d",&init);
-
-	printf("Enter the previous position of head: ");
-	scanf("%d",&prev);
-
-	printf("Enter the cylinder size: ");
-	scanf("%d",&limit);
-	scan(init,prev,req,n,limit);
+       printf("Enter the maximum range of Disk: ");
+    scanf("%d",&maxrange);
+    
+    printf("Enter the number of queue requests: ");
+    scanf("%d",&n);
+    
+    printf("Enter the initial head position: ");
+    scanf("%d",&init);
+    
+    scan(init,n,maxrange);
 	break;
      } 
 }
